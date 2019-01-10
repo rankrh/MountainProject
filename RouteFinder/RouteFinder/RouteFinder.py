bee757a4a1bf508b99026336ebd3a61b63fac2df
Bob Rank <rankrh@gmail.com>
2019-01-10 16:29:00 -0700

    Set up basic outline of search page

--- RouteFinder/RouteFinder/RouteFinder.py
+++ RouteFinder/RouteFinder/RouteFinder.py
@@ -2,8 +2,12 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 
-class RouteFinder(App):
+class SearchPage(BoxLayout):
     pass
+
+class RouteFinder(App):
+    def build(self):
+        return SearchPage()
 
 if __name__ == '__main__':
     RouteFinder().run() 
