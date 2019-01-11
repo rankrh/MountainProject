1dfd4a4178efcf78ec14494151d6904d7d219556
Bob Rank <rankrh@gmail.com>
2019-01-10 22:12:02 -0700

    Created difficulty trackers

--- RouteFinder/RouteFinder/RouteFinder.py
+++ RouteFinder/RouteFinder/RouteFinder.py
@@ -1,9 +1,35 @@
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.rangeslider import RangeSlider
+
 
 class SearchPage(BoxLayout):
-    pass
+
+    styles = {'sport': {'search': False, 'id': 'sport_slide'},
+              'trad': {'search':False, 'id': 'trad_slide'},
+              'tr': {'search': False, 'id': 'tr_slide'},
+              'boulder': {'search': False, 'id': 'boulder_slide'},
+              'mixed': {'search': False, 'id': 'mixed_slide'},
+              'snow': {'search': False, 'id': 'snow_slide'},
+              'aid': {'search': False, 'id': 'aid_slide'},
+              'ice': {'search': False, 'id': 'ice_slide'},
+              'alpine': {'search': False, 'id': 'alpine_slide'}}
+
+    def __init__(self):
+        super(SearchPage, self).__init__()
+
+    def set_style(self, style):
+        self.styles[style]['search'] = not self.styles[style]['search']
+        style_id = self.styles[style]['id']
+        
+        if self.styles[style]['search']:
+            self.ids[style_id].disable = False
+            self.ids[style_id].opacity = 1.0
+        elif not self.styles[style]['search']:
+            self.ids[style_id].disable = True
+            self.ids[style_id].opacity = 0.0
+
 
 class RouteFinder(App):
     def build(self):
