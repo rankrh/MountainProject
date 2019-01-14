9d0f357fa35c86954f5e14b910be75ebc557bf0f
Bob Rank <rankrh@gmail.com>
2019-01-13 17:48:33 -0700

    Created function to select routes from database

--- RouteFinder.py
+++ RouteFinder.py
@@ -1,5 +1,7 @@
 from googlemaps.haversine import Haversine
 from googlemaps.geocode import GeoCode
+import pandas as pd
+import sqlite3
 
 from kivy.uix.screenmanager import ScreenManager
 from kivy.uix.screenmanager import Screen
@@ -10,72 +12,100 @@ from kivy.uix.label import Label
 from kivy.lang import Builder
 from kivy.app import App
 
+conn = sqlite3.connect('Routes-Cleaned.sqlite')
+cursor = conn.cursor()
+
 class StylesPage(Screen):
 
-    styles = {'sport': {'search': False,
-                        'slider_id': 'sport_slide',
-                        'label_id': 'sport_diff'},
-              'trad': {'search': False,
-                       'slider_id': 'trad_slide',
-                       'label_id': 'trad_diff'},
-              'tr': {'search': False,
-                     'slider_id': 'tr_slide',
-                     'label_id': 'tr_diff'},
-              'boulder': {'search': False,
-                          'slider_id': 'boulder_slide',
-                          'label_id': 'boulder_diff'},
-              'mixed': {'search': False,
-                        'slider_id': 'mixed_slide',
-                        'label_id': 'mixed_diff'},
-              'snow': {'search': False,
-                       'slider_id': 'snow_slide',
-                       'label_id': 'snow_diff'},
-              'aid': {'search': False,
-                      'slider_id': 'aid_slide',
-                      'label_id': 'aid_diff'},
-              'ice': {'search': False,
-                      'slider_id': 'ice_slide',
-                      'label_id': 'ice_diff'},
-              'alpine': {'search': False,
-                         'slider_id': 'alpine_slide',
-                         'label_id': 'alpine_diff'}}
+    styles = {
+        'sport': {
+            'search': False,
+            'slider_id': 'sport_slide',
+            'label_id': 'sport_diff',
+            'grades': (None, None)}, 
+        'trad': {
+            'search': False,
+            'slider_id': 'trad_slide',
+            'label_id': 'trad_diff',
+            'grades': (None, None)},
+        'tr': {
+            'search': False,
+            'slider_id': 'tr_slide',
+            'label_id': 'tr_diff',
+            'grades': (None, None)},
+        'boulder': {
+            'search': False,
+            'slider_id': 'boulder_slide',
+            'label_id': 'boulder_diff',
+            'grades': (None, None)},
+        'mixed': {
+            'search': False,
+            'slider_id': 'mixed_slide',
+            'label_id': 'mixed_diff',
+            'grades': (None, None)},
+        'snow': {
+            'search': False,
+            'slider_id': 'snow_slide',
+            'label_id': 'snow_diff',
+            'grades': (None, None)},
+        'aid': {
+            'search': False,
+            'slider_id': 'aid_slide',
+            'label_id': 'aid_diff',
+            'grades': (None, None)},
+        'ice': {
+            'search': False,
+            'slider_id': 'ice_slide',
+            'label_id': 'ice_diff',
+            'grades': (None, None)},
+        'alpine': {
+            'search': False,
+            'slider_id': 'alpine_slide',
+            'label_id': 'alpine_diff',
+            'grades': (None, None)}}
 
-    rope_conv = ['3rd', '4th', 'Easy 5th', '5.0', '5.1', '5.2', '5.3',
-                    '5.4', '5.5', '5.6', '5.7', '5.7+', '5.8-', '5.8', '5.8+',
-                    '5.9-', '5.9', '5.9+', '5.10a', '5.10-', '5.10a/b',
-                    '5.10b', '5.10', '5.10b/c', '5.10c', '5.10+', '5.10c/d',
-                    '5.10d', '5.11a', '5.11-', '5.11a/b', '5.11b', '5.11',
-                    '5.11b/c', '5.11c', '5.11+', '5.11c/d', '5.11d', '5.12a',
-                    '5.12-', '5.12a/b', '5.12b', '5.12', '5.12b/c', '5.12c',
-                    '5.12+', '5.12c/d', '5.12d', '5.13a', '5.13-', '5.13a/b',
-                    '5.13b', '5.13', '5.13b/c', '5.13c', '5.13+', '5.13c/d',
-                    '5.13d', '5.14a', '5.14-', '5.14a/b', '5.14b', '5.14',
-                    '5.14b/c', '5.14c', '5.14+', '5.14c/d', '5.14d', '5.15a',
-                    '5.15-', '5.15a/b', '5.15b', '5.15', '5.15c', '5.15+',
-                    '5.15c/d', '5.15d']
-    boulder_conv  = ['V-easy', 'V0-', 'V0', 'V0+', 'V0-1', 'V1-', 'V1',
-                        'V1+', 'V1-2', 'V2-', 'V2', 'V2+', 'V2-3', 'V3-',
-                        'V3', 'V3+', 'V3-4', 'V4-', 'V4', 'V4+', 'V4-5',
-                        'V5-', 'V5', 'V5+', 'V5-6', 'V6-', 'V6', 'V6+',
-                        'V6-7', 'V7-', 'V7', 'V7+', 'V7-8', 'V8-', 'V8',
-                        'V8+', 'V8-9', 'V9-', 'V9', 'V9+', 'V9-10', 'V10-',
-                        'V10', 'V10+', 'V10-11', 'V11-', 'V11', 'V11+',
-                        'V11-12', 'V12-', 'V12', 'V12+', 'V12-13', 'V13-', 
-                        'V13', 'V13+', 'V13-14', 'V14-', 'V14', 'V14+',
-                        'V14-15', 'V15-', 'V15', 'V15+', 'V15-16', 'V16-',
-                        'V16', 'V16+', 'V16-17', 'V17-', 'V17']
-    mixed_conv = ['M1', 'M2', 'M3', 'M4', 'M5', 'M6', 'M7', 'M8', 'M9',
-                    'M10', 'M11', 'M12']
-    aid_conv = ['A0', 'A1', 'A2', 'A2+', 'A3',
-                'A3+', 'A4', 'A4+', 'A5','A6']
+    rope_conv = [
+        '3rd', '4th', 'Easy 5th', '5.0', '5.1', '5.2', '5.3', '5.4', '5.5',
+        '5.6', '5.7', '5.7+', '5.8-', '5.8', '5.8+', '5.9-', '5.9', '5.9+',
+        '5.10a', '5.10-', '5.10a/b', '5.10b', '5.10', '5.10b/c', '5.10c',
+        '5.10+', '5.10c/d', '5.10d', '5.11a', '5.11-', '5.11a/b', '5.11b',
+        '5.11', '5.11b/c', '5.11c', '5.11+', '5.11c/d', '5.11d', '5.12a', 
+        '5.12-', '5.12a/b', '5.12b', '5.12', '5.12b/c', '5.12c', '5.12+',
+        '5.12c/d', '5.12d', '5.13a', '5.13-', '5.13a/b', '5.13b', '5.13',
+        '5.13b/c', '5.13c', '5.13+', '5.13c/d', '5.13d', '5.14a', '5.14-',
+        '5.14a/b', '5.14b', '5.14', '5.14b/c', '5.14c', '5.14+', '5.14c/d',
+        '5.14d', '5.15a', '5.15-', '5.15a/b', '5.15b', '5.15', '5.15c',
+        '5.15+', '5.15c/d', '5.15d']
+
+    boulder_conv  = [
+        'V-easy', 'V0-', 'V0', 'V0+', 'V0-1', 'V1-', 'V1', 'V1+', 'V1-2',
+        'V2-', 'V2', 'V2+', 'V2-3', 'V3-', 'V3', 'V3+', 'V3-4', 'V4-', 'V4',
+        'V4+', 'V4-5', 'V5-', 'V5', 'V5+', 'V5-6', 'V6-', 'V6', 'V6+', 'V6-7',
+        'V7-', 'V7', 'V7+', 'V7-8', 'V8-', 'V8', 'V8+', 'V8-9', 'V9-', 'V9',
+        'V9+', 'V9-10', 'V10-', 'V10', 'V10+', 'V10-11', 'V11-', 'V11', 'V11+',
+        'V11-12', 'V12-', 'V12', 'V12+', 'V12-13', 'V13-', 'V13', 'V13+',
+        'V13-14', 'V14-', 'V14', 'V14+', 'V14-15', 'V15-', 'V15', 'V15+',
+        'V15-16', 'V16-', 'V16', 'V16+', 'V16-17', 'V17-', 'V17']
+    
+    mixed_conv = [
+        'M1', 'M2', 'M3', 'M4', 'M5', 'M6', 'M7', 'M8', 'M9', 'M10', 'M11',
+        'M12']
+    
+    aid_conv = ['A0', 'A1', 'A2', 'A2+', 'A3', 'A3+', 'A4', 'A4+', 'A5','A6']
     ice_conv = ['WI1', 'WI2', 'WI3', 'WI4', 'WI5', 'WI6', 'WI7', 'WI8']
     snow_conv = ['Easy', 'Mod', 'Steep']
     alp_conv =  ['I', 'II', 'III', 'IV', 'V', 'VI']
 
-    conversion = {'sport': rope_conv, 'trad': rope_conv, 'tr': rope_conv,
-                    'boulder': boulder_conv, 'mixed': mixed_conv,
-                    'aid': aid_conv, 'ice': ice_conv, 'snow': snow_conv,
-                    'alpine': alp_conv}
+    conversion = {
+        'sport': rope_conv,
+        'trad': rope_conv,
+        'tr': rope_conv,
+        'boulder': boulder_conv,
+        'mixed': mixed_conv,
+        'aid': aid_conv,
+        'ice': ice_conv,
+        'snow': snow_conv,
+        'alpine': alp_conv}
 
     def set_style(self, style):
         self.styles[style]['search'] = not self.styles[style]['search']
@@ -94,6 +124,7 @@ class StylesPage(Screen):
     def difficulty_conversion(self, style, difficulty_range):
         low = int(difficulty_range[0])
         high = int(difficulty_range[1])
+        self.styles[style]['grades'] = (low, high)
         
         grades = self.conversion[style]
         if high == 100:
@@ -109,19 +140,23 @@ class StylesPage(Screen):
 
 class PreferencesPage(Screen):
     pitches = False
-    preferences = {'pitches': None,
-                   'danger': 3,
-                   'location': {'name': None,
-                                'coordinates': None},
-                   'features': {'Arete': False,
-                                'Chimney': False,
-                                'Crack': False,
-                                'Slab': False,
-                                'Overhang': False}}
+    preferences = {
+        'pitches': None, 
+        'danger': 3, 
+        'location': {
+            'name': None,
+            'coordinates': None},
+        'features': {
+            'Arete': False,
+            'Chimney': False,
+            'Crack': False,
+            'Slab': False,
+            'Overhang': False}}
     
     def set_up(self, styles):
-        multipitch_styles = ['sport', 'trad', 'aid', 'mixed',
-                             'alpine', 'snow', 'ice']
+        multipitch_styles = [
+            'sport', 'trad', 'aid', 'mixed', 'alpine', 'snow', 'ice']
+        
         for style in multipitch_styles:
             if style in styles.keys():
                 if styles[style]['search']:
@@ -201,6 +236,7 @@ class ResultsPage(Screen):
                                               'Crack': False,
                                               'Slab': False,
                                               'Overhang': False}}}
+        
     def get_routes(self, styles, preferences):
         location = preferences['location']
         location_name = location['name']
@@ -209,13 +245,40 @@ class ResultsPage(Screen):
         else:
             location['name'] = None
 
-        chosen = []
+        grades = {
+            'sport': 'rope_conv', 
+            'trad': 'rope_conv',
+            'tr': 'rope_conv',
+            'boulder': 'boulder_conv',
+            'mixed': 'mixed_conv',
+            'snow': 'snow_conv',
+            'aid': 'aid_conv',
+            'ice': 'ice_conv',
+            'alpine': 'nccs_conv'}
+        
+        query = 'SELECT * FROM Routes WHERE '
+        unwanted = ''
+
         for style, data in styles.items():
             if data['search']:
-                chosen.append(style)
-        
+                conversion = grades[style]
+                low_grade = data['grades'][0]
+                high_grade = data['grades'][1]
 
-        self.ids.test.text = str(chosen)
+                keys = (style, conversion, low_grade, high_grade)
+                text = '(%s = 1 AND %s BETWEEN %s AND %s) OR ' % keys
+
+                query += text
+            elif not data['search'] and style is not 'tr':
+                unwanted += 'AND ' + style + ' = 0 '
+                
+        query = query[:-3]
+        query += unwanted
+
+        routes = pd.read_sql(query, con=conn)
+        print(routes)
+
+        self.ids.test.text = query
 
         
     
